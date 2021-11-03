e71634c93c9f6e29c968d66f75d3e1f94a1fc3cb
Jonathan <jonathangani279@gmail.com>
2021-11-03 20:53:42 +0700

    create suicide terms story

--- actions/actions.py
+++ actions/actions.py
@@ -11,6 +11,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, FormValidationAction, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
+
 import requests
 
 from utilities.helpers import extract_entity
